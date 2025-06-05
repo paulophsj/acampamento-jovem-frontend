c64c43a9b578fd5754e782bbbd8e47d0d20eae95
paulophsj <snoopyphs@gmail.com>
2025-06-05 11:29:30 -0300

    Resolvendo cookie

--- middleware.js
+++ middleware.js
@@ -1,14 +1,16 @@
 import { NextRequest, NextResponse } from 'next/server'
 
 export function middleware(request) {
-  const isLoggedIn = request.cookies.has('access_token')
+  const isLoggedIn = request.cookies.get('access_token')?.value
   const pathname = request.nextUrl.pathname
+
+  console.log(isLoggedIn)
 
   if (pathname.startsWith('/login') && isLoggedIn) {
     return NextResponse.redirect(new URL('/', request.url))
   }
 
-  const protectedRoutes = ['/dashboard', '/admin', '/perfil', '/user']
+  const protectedRoutes = ['/dashboard', '/admin', '/user']
   const isProtected = protectedRoutes.some(route => pathname.startsWith(route))
 
   if (isProtected && !isLoggedIn) {
