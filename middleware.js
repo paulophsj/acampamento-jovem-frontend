873f2f0d2ba2f50c8fc5a5796703b30e71365f87
paulophsj <snoopyphs@gmail.com>
2025-06-05 13:27:10 -0300

    A vercel não aceita cookies de subdominios

--- middleware.js
+++ middleware.js
@@ -1,7 +1,7 @@
 import { NextRequest, NextResponse } from 'next/server'
 
 export function middleware(request) {
-  const isLoggedIn = request.cookies.getAll()[0].value
+  const isLoggedIn = request.cookies.has('access_key')
   const pathname = request.nextUrl.pathname
 
   if (pathname.startsWith('/login') && isLoggedIn) {
