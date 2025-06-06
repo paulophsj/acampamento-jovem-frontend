9e021f3f8291b44f9809f687602f1ada4b6494bb
paulophsj <snoopyphs@gmail.com>
2025-06-05 22:40:20 -0300

    Ajustando middleware

--- middleware.js
+++ middleware.js
@@ -1,10 +1,11 @@
 import { NextRequest, NextResponse } from 'next/server'
 
 export function middleware(request) {
-  const isLoggedIn = request.cookies.getAll()
+  const isLoggedIn = request.cookies.get("access_token")
   const pathname = request.nextUrl.pathname
 
-  console.lot(isLoggedIn)
+  console.log("Access_Token" + isLoggedIn)
+  console.log("All cookies" + request.cookies.getAll())
 
   if (pathname.startsWith('/login') && isLoggedIn) {
     return NextResponse.redirect(new URL('/', request.url))
